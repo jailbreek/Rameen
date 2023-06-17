6ae96ac4296e1201431b32f28445cde30881373a
cpt_n3m0 <90511215+Jailbreek@users.noreply.github.com>
2023-06-17 22:22:26 +0700

    add database and controller

---
+++ Controller/RamenController.cs
@@ -0,0 +1,31 @@
+﻿using Raameenn.Handler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Raameenn.Controller
+{
+    public class RamenController
+    {
+        private static bool invertBool(bool x)
+        {
+            return !x;
+        }
+        public static string addRamen(string name, string meatId, string broth, string price)
+        {
+            if (broth.Equals("") || name.Equals("") || meatId.Equals("") || price.Equals("")) return "please fill all the fied!";
+            if (invertBool(name.Contains("Ramen"))) return "Ramen name must contain 'Ramen'";
+            if (int.Parse(price) < 3000) return "Minimum Price is 3000";
+            return RamenHandler.addRamen(name, meatId, broth, price);
+
+        }
+        public static string editRamen(string id, string name, string meatId, string broth, string price)
+        {
+            if (broth.Equals("") || name.Equals("") || meatId.Equals("") || price.Equals("") || id.Equals("")) return "please fill all the fied!";
+            if (invertBool(name.Contains("Ramen"))) return "Ramen name must contain 'Ramen'";
+            if (int.Parse(price) < 3000) return "Minimum Price is 3000";
+            return RamenHandler.editRamen(id, name, meatId, broth, price);
+        }
+    }
+}
---
+++ Controller/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Raameenn.Factory;
+using Raameenn.Handler;
+using Raameenn.Model;
+using Raameenn.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Raameenn.Controller
+{
+    public class UserController
+    {
+        public static void RegisUser(string username, string email, string gender, int roleId, string password)
+        {
+            UserHandler.RegisUser(username, email, gender, roleId, password);
+        }
+        public static User Login(string username, string password)
+        {
+            return UserHandler.GetUser(username, password);
+        }
+        public static List<User> GetAllUserByRole(string role)
+        {
+            return UserHandler.GetUserByRole(role);
+        }
+        public static bool Update(int id, string new_username, string new_email, string new_gender)
+        {
+            return UserHandler.UpdateUser(id, new_username, new_email, new_gender);
+        }
+        public static User GetUserById(int id)
+        {
+            return UserHandler.GetUserById(id);
+        }
+    }
+}
